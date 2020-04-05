9facf72832960b5540743b63ebeeb471ca92abd0
Alex Stokes <r.alex.stokes@gmail.com>
2020-04-05 14:29:48 -0700

    Refactor and finish most of first pass

--- deposit_contract_proxy.sol
+++ deposit_contract_proxy.sol
@@ -4,22 +4,13 @@ interface DepositContract {
     function deposit(bytes, bytes32, bytes, bytes32) external payable;
 }
 
-
-contract DepositContractProxy  {
+library DepositSSZ {
     uint constant WEI_PER_GWEI = 1e9;
-    uint constant PUBLIC_KEY_LENGTH = 48;
-    uint constant SIGNATURE_LENGTH = 96;
     // Constant related to versioning serializations of deposits on eth2
     bytes32 constant DEPOSIT_DOMAIN = 0x03000000f5a5fd42d16a20302798ef6ed309979b43003d2320d9f0e8ea9831a9;
 
-    DepositContract depositContract;
-
-    constructor(address _depositContractAddress) public {
-        depositContract = DepositContract(_depositContractAddress);
-    }
-
     // Return a `wei` value in units of Gwei and serialize as a (LE) `bytes8`.
-    function serializeAmount(uint amount) pure returns (bytes memory) {
+    function serializeAmount(uint amount) private pure returns (bytes memory) {
         uint depositAmount = amount / WEI_PER_GWEI;
 
         bytes memory encodedAmount = new bytes(8);
@@ -31,19 +22,14 @@ contract DepositContractProxy  {
         return encodedAmount;
     }
 
-    // TODO fix comments
-    // Compute the "signing root" from the deposit message. This root is the Merkle root of a specific tree
-    // specified by SSZ serialization that takes as leaves chunks of 32 bytes.
-    //             |--------------- 32 bytes ---------------|
-    // input data: |-------- publicKey: byte[48] --------|
-    // padded data to yield leaves
-    // and then recursively hash pairs of leaves until there is only one value.
+    // Compute the "signing root" from the deposit message. This root is the Merkle root
+    // of a specific tree specified by SSZ serialization that takes as leaves chunks of 32 bytes.
     // NOTE: This computation is done manually in ``computeSigningRoot``.
     function computeSigningRoot(
         bytes memory publicKey,
         bytes32 withdrawalCredentials,
         uint amount
-    ) pure returns (bytes32) {
+    ) internal pure returns (bytes32) {
         bytes memory serializedAmount = serializeAmount(amount);
 
         bytes memory serializedPublicKey = new bytes(64);
@@ -65,31 +51,142 @@ contract DepositContractProxy  {
 
         return sha256(abi.encodePacked(depositMessageRoot, DEPOSIT_DOMAIN));
     }
+}
 
-    function mapToField(bytes32 message) pure returns (bytes memory) {
-        // TODO what is the latest map to field?
+library BLSSignature {
+    // NOTE: precompile addresses are placeholders
+    address constant BLS12_381_PAIRING_PRECOMPILE_ADDRESS = 0xA;
+    address constant BLS12_381_MAP_FIELD_TO_CURVE_PRECOMPILE_ADDRESS = 0xB;
+
+    // Fp is a field element with the high-order part stored in `a`.
+    struct Fp {
+        uint a;
+        uint b;
+    };
+
+    // Fp2 is an extension field element with the coefficient of the
+    // quadratic non-residue stored in `b`, i.e. p = a + i * b
+    struct Fp2 {
+        Fp a;
+        Fp b;
     }
 
-    function mapToCurve(bytes memory fieldElement) pure returns (bytes memory) {
-        // TODO call precompile for map to curve
+    // G1Point represents a point on BLS12-381 over Fp with coordinates (X,Y);
+    struct G1Point {
+        Fp X;
+        Fp Y;
     }
 
-    function paring(bytes memory a, bytes memory b) pure returns (bytes memory) {
-        // TODO call precompile for pairing operation
-        // TODO is there a shortcut for a pairing equality check?
+    // G2Point represents a point on BLS12-381 over Fp2 with coordinates (X,Y);
+    struct G2Point {
+        Fp2 X;
+        Fp2 Y;
     }
 
-    function validBLSSignature(
+    // Returns an element in Fp2
+    function hashToField(bytes32 message) private pure returns (Fp2 result) {
+        // TODO implement v6 hash to field
+        result.a.a = sha256(message);
+        result.a.b = sha256(message);
+        result.b.a = sha256(message);
+        result.b.b = sha256(message);
+        return;
+    }
+
+    function mapToCurve(Fp2 fieldElement) private view returns (G2Point point) {
+        bool success;
+        assembly {
+            success := staticcall(
+                sub(gas, 2000),
+                BLS12_381_MAP_FIELD_TO_CURVE_PRECOMPILE_ADDRESS,
+                fieldElement,
+                128,
+                point,
+                256,
+            )
+        }
+        require(success, "call to map to curve precompile failed");
+    }
+
+    function paring(G1Point u, G2Point v) private view returns (bytes32 result) {
+        uint[12] memory input;
+
+        input[0] =  u.X.a;
+        input[1] =  u.X.b;
+        input[2] =  u.Y.a;
+        input[3] =  u.Y.b;
+
+        input[4] =  v.X.a.a;
+        input[5] =  v.X.a.b;
+        input[6] =  v.X.b.a;
+        input[7] =  v.X.b.b;
+        input[8] =  v.Y.a.a;
+        input[9] =  v.Y.a.b;
+        input[10] = v.Y.b.a;
+        input[11] = v.Y.b.b;
+
+        bool success;
+        assembly {
+            success := staticcall(
+                sub(gas, 2000),
+                BLS12_381_PAIRING_PRECOMPILE_ADDRESS,
+                input,
+                384,
+                result,
+                32,
+            )
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid }
+        }
+        require(success, "call to pairing precompile failed");
+    }
+
+    // Return the generator of G1.
+    function P1() private pure returns (G1Point) {
+        return G1Point(
+            Fp(
+                31827880280837800241567138048534752271,
+                88385725958748408079899006800036250932223001591707578097800747617502997169851,
+            ),
+            Fp(
+                11568204302792691131076548377920244452,
+                114417265404584670498511149331300188430316142484413708742216858159411894806497
+            ),
+        );
+    }
+
+    function decodePublicKey(bytes memory encodedPublicKey) private pure returns (G1Point) {
+        // TODO the decoding...
+        return G1Point(Fp(0,0),Fp(0,0));
+    }
+
+    function decodeSignature(bytes memory encodedSignature) private pure returns (G2Point) {
+        // TODO the decoding...
+        return G2Point(Fp2(Fp(0,0), Fp(0,0)), Fp2(Fp(0,0), Fp(0,0)));
+    }
+
+    function isValid(
         bytes32 message,
-        bytes memory publicKey,
-        bytes memory signature
-    ) pure returns (bool) {
-        bytes memory messageInField = mapToField(message);
-        bytes memory messageOnCurve = mapToCurve(messageInField);
-        // TODO define generator
-        generator = new bytes(48);
-        // TODO verify ordering of the calls
-        return pairing(signature, generator) == pairing(messageOnCurve, publicKey);
+        bytes memory encodedPublicKey,
+        bytes memory encodedSignature
+    ) internal returns (bool) {
+        G1Point publicKey = decodePublicKey(encodedPublicKey);
+        G2Point signature = decodeSignature(encodedSignature);
+
+        Fp2 messageInField = hashToField(message);
+        G2Point messageOnCurve = mapToCurve(messageInField);
+        return pairing(publicKey, messageOnCurve) == pairing(P1(), signature);
+    }
+}
+
+contract DepositContractProxy  {
+    uint constant PUBLIC_KEY_LENGTH = 48;
+    uint constant SIGNATURE_LENGTH = 96;
+
+    DepositContract depositContract;
+
+    constructor(address depositContractAddress) public {
+        depositContract = DepositContract(depositContractAddress);
     }
 
     function verifyAndDeposit(
@@ -101,10 +198,10 @@ contract DepositContractProxy  {
         require(publicKey.length == PUBLIC_KEY_LENGTH, "incorrectly sized public key");
         require(signature.length == SIGNATURE_LENGTH, "incorrectly sized signature");
 
-        bytes32 signingRoot = computeSigningRoot(publicKey, withdrawalCredentials, msg.value);
+        bytes32 signingRoot = DepositSSZ.computeSigningRoot(publicKey, withdrawalCredentials, msg.value);
 
         require(
-            validBLSSignature(signingRoot, publicKey, signature),
+            BLSSignature.isValid(signingRoot, publicKey, signature),
             "invalid BLS signature given deposit data"
         );
 
