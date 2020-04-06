fed616248645de8632992000b91cd637ccae1921
Alex Stokes <r.alex.stokes@gmail.com>
2020-04-05 17:51:55 -0700

    wip: implement hash to curve

--- deposit_contract_proxy.sol
+++ deposit_contract_proxy.sol
@@ -57,6 +57,9 @@ library BLSSignature {
     // NOTE: precompile addresses are placeholders
     address constant BLS12_381_PAIRING_PRECOMPILE_ADDRESS = 0xA;
     address constant BLS12_381_MAP_FIELD_TO_CURVE_PRECOMPILE_ADDRESS = 0xB;
+    address constant BLS12_381_G2_ADD_ADDRESS = 0xC;
+    address constant BLS12_381_G2_MULTIPLY_ADDRESS = 0xD;
+    string constant BLS_SIG_DST = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_";
 
     // Fp is a field element with the high-order part stored in `a`.
     struct Fp {
@@ -83,29 +86,102 @@ library BLSSignature {
         Fp2 Y;
     }
 
-    // Returns an element in Fp2
-    function hashToField(bytes32 message) private pure returns (Fp2 result) {
-        // TODO implement v6 hash to field
-        result.a.a = sha256(message);
-        result.a.b = sha256(message);
-        result.b.a = sha256(message);
-        result.b.b = sha256(message);
+    function expandMessage(bytes32 message) private pure returns (bytes memory) {
+        // uint LEN_IN_BYTES = 256;
+        // TODO implement `expand_message_xmd` with `message`, `DST` and `LEN_IN_BYTES`
+        return abi.encodePacked(
+            sha256(message),
+            sha256(message),
+            sha256(message),
+            sha256(message),
+            sha256(message),
+            sha256(message),
+            sha256(message),
+            sha256(message),
+        );
+    }
+
+    function convertBytesToFp(bytes memory data, uint start, uint end) private pure returns (Fp) {
+        Fp result;
+        // TODO
+        // read data[start:end]
+        // convert to some number, modulo FIELD_MODULUS
+        // split in two and make Fp
+        return result;
+    }
+
+    function hashToField(bytes32 message) private pure returns (Fp2[2] memory result) {
+        bytes memory some_bytes = expandMessage(message);
+        result[0] = Fp2(
+            convertBytesToFp(some_bytes, 0, 64),
+            convertBytesToFp(some_bytes, 64, 128)
+        );
+        result[1] = Fp2(
+            convertBytesToFp(some_bytes, 128, 192),
+            convertBytesToFp(some_bytes, 192, 256)
+        );
         return;
     }
 
-    function mapToCurve(Fp2 fieldElement) private view returns (G2Point point) {
+    function mapToCurve(Fp2 memory input) private view returns (G2Point result) {
         bool success;
         assembly {
             success := staticcall(
                 sub(gas, 2000),
                 BLS12_381_MAP_FIELD_TO_CURVE_PRECOMPILE_ADDRESS,
-                fieldElement,
+                input,
                 128,
-                point,
+                result,
                 256,
             )
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid }
         }
         require(success, "call to map to curve precompile failed");
+    }
+
+    function addG2(G2Point a, G2Point b) private view returns (G2Point result) {
+        uint[16] memory input;
+        input[0]  = a.X.a.a;
+        input[1]  = a.X.a.b;
+        input[2]  = a.X.b.a;
+        input[3]  = a.X.b.b;
+        input[4]  = a.Y.a.a;
+        input[5]  = a.Y.a.a;
+        input[6]  = a.Y.a.b;
+        input[7]  = a.Y.b.a;
+
+        input[8]  = b.X.a.a;
+        input[9]  = b.X.a.b;
+        input[10] = b.X.b.a;
+        input[11] = b.X.b.b;
+        input[12] = b.Y.a.a;
+        input[13] = b.Y.a.a;
+        input[14] = b.Y.a.b;
+        input[15] = b.Y.b.a;
+
+        bool success;
+        assembly {
+            success := staticcall(
+                sub(gas, 2000),
+                BLS12_381_G2_ADD_ADDRESS,
+                input,
+                512,
+                result,
+                256,
+            )
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid }
+        }
+        require(success, "call to addition in G2 precompile failed");
+    }
+
+    // Implements v6 of "hash to the curve" of the IETF BLS draft.
+    function hashToCurve(bytes32 message) private view returns (G2Point) {
+        Fp2[2] memory messageElementsInField = hashToField(message);
+        G2Point firstPoint = mapToCurve(messageElementsInField[0]);
+        G2Point secondPoint = mapToCurve(messageElementsInField[1]);
+        return addG2(firstPoint, secondPoint);
     }
 
     function paring(G1Point u, G2Point v) private view returns (bytes32 result) {
@@ -155,24 +231,43 @@ library BLSSignature {
         );
     }
 
+    function convertBEBytesToUint(bytes memory data) private pure returns (uint) {
+        assert(data.length <= 32);
+
+        uint result;
+        for (uint i = 0; i < data.length; i++) {
+            byte b = data[i];
+            result = result + (uint8(b) * 2**(8*(data.length-i-1)));
+        }
+        return result;
+    }
+
+    function sliceToUint(bytes memory data, uint start, uint end) private pure returns (uint) {
+        uint length = end - start;
+        assert(length >= 0);
+
+        bytes memory slice = new bytes(length);
+        for (uint i = 0; i < length; i++) {
+            slice[i] = data[start+i]
+        }
+        return convertBEBytesToUint(slice);
+    }
+
     function decodeG1Point(bytes memory encodedX, Fp Y) private pure returns (G1Point) {
-        Fp X = Fp(
-            uint(encodedX[32:48]),
-            uint(encodedX[0:32]),
-        );
+        uint a = sliceToUint(encodedX, 32, 48);
+        uint b = sliceToUint(encodedX, 0, 32);
+        Fp X = Fp(a, b);
         return G1Point(X,Y);
     }
 
     function decodeG2Point(bytes memory encodedX, Fp2 Y) private pure returns (G2Point) {
+        uint aa = sliceToUint(encodedX, 32, 48);
+        uint ab = sliceToUint(encodedX, 0, 32);
+        uint ba = sliceToUint(encodedX, 80, 96);
+        uint bb = sliceToUint(encodedX, 48, 80);
         Fp2 X = Fp2(
-            Fp(
-                uint(encodedX[32:48]),
-                uint(encodedX[0:32]),
-            ),
-            Fp(
-                uint(encodedX[80:96]),
-                uint(encodedX[48:80]),
-            )
+            Fp(aa,ab),
+            Fp(ba, bb),
         );
         return G2Point(X, Y);
     }
@@ -183,12 +278,11 @@ library BLSSignature {
         bytes memory encodedSignature,
         Fp publicKeyYCoordinate,
         Fp2 signatureYCoordinate,
-    ) internal returns (bool) {
+    ) internal view returns (bool) {
         G1Point publicKey = decodeG1Point(encodedPublicKey, publicKeyYCoordinate);
         G2Point signature = decodeG2Point(encodedSignature, signatureYCoordinate);
 
-        Fp2 messageInField = hashToField(message);
-        G2Point messageOnCurve = mapToCurve(messageInField);
+        G2Point messageOnCurve = hashToCurve(message);
         return pairing(publicKey, messageOnCurve) == pairing(P1(), signature);
     }
 }
