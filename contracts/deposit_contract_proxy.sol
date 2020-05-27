ec02f91c2c9ef8d9dde1d11ad2cc25fe93c0b59f
Alex Stokes <r.alex.stokes@gmail.com>
2020-05-27 08:28:33 -0700

    Add basic unit testing
    Fixes bugs in deposit proxy contract found while getting tests to pass

--- contracts/deposit_contract_proxy.sol
+++ contracts/deposit_contract_proxy.sol
@@ -1,73 +1,30 @@
-pragma solidity ^0.6.6;
+// SPDX-License-Identifier: The Unlicense
+pragma solidity ^0.6.8;
 pragma experimental ABIEncoderV2;
 
 interface DepositContract {
     function deposit(
         bytes calldata publicKey,
-        bytes32 withdrawalCredentials,
+        bytes calldata withdrawalCredentials,
         bytes calldata signature,
         bytes32 depositDataRoot
     ) external payable;
 }
 
-library DepositSSZ {
+contract DepositContractProxy  {
     uint constant PUBLIC_KEY_LENGTH = 48;
+    uint constant SIGNATURE_LENGTH = 96;
 
     uint constant WEI_PER_GWEI = 1e9;
     // Constant related to versioning serializations of deposits on eth2
     bytes32 constant DEPOSIT_DOMAIN = 0x03000000f5a5fd42d16a20302798ef6ed309979b43003d2320d9f0e8ea9831a9;
 
-    // Return a `wei` value in units of Gwei and serialize as a (LE) `bytes8`.
-    function serializeAmount(uint amount) private pure returns (bytes memory) {
-        uint depositAmount = amount / WEI_PER_GWEI;
-
-        bytes memory encodedAmount = new bytes(8);
-
-        for (uint i = 0; i < 8; i++) {
-            encodedAmount[i] = byte(uint8(depositAmount / (2**(8*i))));
-        }
-
-        return encodedAmount;
-    }
-
-    // Compute the "signing root" from the deposit message. This root is the Merkle root
-    // of a specific tree specified by SSZ serialization that takes as leaves chunks of 32 bytes.
-    // NOTE: This computation is done manually in ``computeSigningRoot``.
-    function computeSigningRoot(
-        bytes memory publicKey,
-        bytes32 withdrawalCredentials,
-        uint amount
-    ) internal pure returns (bytes32) {
-        bytes memory serializedAmount = serializeAmount(amount);
-
-        bytes memory serializedPublicKey = new bytes(64);
-        for (uint i = 0; i < PUBLIC_KEY_LENGTH; i++) {
-            serializedPublicKey[i] = publicKey[i];
-        }
-
-        bytes32 publicKeyRoot = sha256(serializedPublicKey);
-        bytes32 firstNode = sha256(abi.encodePacked(publicKeyRoot, withdrawalCredentials));
-
-        bytes memory restOfChunks = new bytes(64);
-        for (uint i = 0; i < 8; i++) {
-            restOfChunks[i] = serializedAmount[i];
-        }
-
-        bytes32 secondNode = sha256(restOfChunks);
-
-        bytes32 depositMessageRoot = sha256(abi.encodePacked(firstNode, secondNode));
-
-        return sha256(abi.encodePacked(depositMessageRoot, DEPOSIT_DOMAIN));
-    }
-}
-
-library BLSSignature {
-    // NOTE: BLS12-381 precompile addresses are placeholders
-    uint8 constant BLS12_381_PAIRING_PRECOMPILE_ADDRESS = 0xA;
-    uint8 constant BLS12_381_MAP_FIELD_TO_CURVE_PRECOMPILE_ADDRESS = 0xB;
-    uint8 constant BLS12_381_G2_ADD_ADDRESS = 0xC;
-    uint8 constant BLS12_381_G2_MULTIPLY_ADDRESS = 0xD;
+    uint8 constant BLS12_381_PAIRING_PRECOMPILE_ADDRESS = 0x10;
+    uint8 constant BLS12_381_MAP_FIELD_TO_CURVE_PRECOMPILE_ADDRESS = 0x12;
+    uint8 constant BLS12_381_G2_ADD_ADDRESS = 0xD;
     string constant BLS_SIG_DST = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_+";
+    bytes1 constant BLS_BYTE_WITHOUT_FLAGS_MASK = bytes1(0x1f);
+
     uint8 constant MOD_EXP_PRECOMPILE_ADDRESS = 0x5;
 
     // Fp is a field element with the high-order part stored in `a`.
@@ -95,7 +52,56 @@ library BLSSignature {
         Fp2 Y;
     }
 
-    function expandMessage(bytes32 message) private pure returns (bytes memory) {
+    DepositContract depositContract;
+
+    constructor(address depositContractAddress) public {
+        depositContract = DepositContract(depositContractAddress);
+    }
+
+    // Return a `wei` value in units of Gwei and serialize as a (LE) `bytes8`.
+    function serializeAmount(uint amount) private pure returns (bytes memory) {
+        uint depositAmount = amount / WEI_PER_GWEI;
+
+        bytes memory encodedAmount = new bytes(8);
+
+        for (uint i = 0; i < 8; i++) {
+            encodedAmount[i] = byte(uint8(depositAmount / (2**(8*i))));
+        }
+
+        return encodedAmount;
+    }
+
+    // Compute the "signing root" from the deposit message. This root is the Merkle root
+    // of a specific tree specified by SSZ serialization that takes as leaves chunks of 32 bytes.
+    // NOTE: This computation is done manually in ``computeSigningRoot``.
+    // NOTE: function is exposed for testing...
+    function computeSigningRoot(
+        bytes memory publicKey,
+        bytes32 withdrawalCredentials,
+        uint amount
+    ) public pure returns (bytes32) {
+        bytes memory serializedPublicKey = new bytes(64);
+        for (uint i = 0; i < PUBLIC_KEY_LENGTH; i++) {
+            serializedPublicKey[i] = publicKey[i];
+        }
+
+        bytes32 publicKeyRoot = sha256(serializedPublicKey);
+        bytes32 firstNode = sha256(abi.encodePacked(publicKeyRoot, withdrawalCredentials));
+
+        bytes memory amountRoot = new bytes(64);
+        bytes memory serializedAmount = serializeAmount(amount);
+        for (uint i = 0; i < 8; i++) {
+            amountRoot[i] = serializedAmount[i];
+        }
+        bytes32 secondNode = sha256(amountRoot);
+
+        bytes32 depositMessageRoot = sha256(abi.encodePacked(firstNode, secondNode));
+        return sha256(abi.encodePacked(depositMessageRoot, DEPOSIT_DOMAIN));
+    }
+
+
+    // NOTE: function exposed for testing...
+    function expandMessage(bytes32 message) public pure returns (bytes memory) {
         bytes memory b0Input = new bytes(143);
         for (uint i = 0; i < 32; i++) {
             b0Input[i+64] = message[i];
@@ -161,7 +167,7 @@ library BLSSignature {
             // base
             // first, copy slice by chunks of EVM words
             let ctr := length
-            let src := add(data, 0x20)
+            let src := add(add(data, 0x20), start)
             let dst := add(p, 0x60)
             for { }
                 or(gt(ctr, 0x20), eq(ctr, 0x20))
@@ -198,12 +204,13 @@ library BLSSignature {
 
     function convertSliceToFp(bytes memory data, uint start, uint end) private view returns (Fp memory) {
         bytes memory fieldElement = reduceModulo(data, start, end);
-        uint a = sliceToUint(fieldElement, 32, 48);
-        uint b = sliceToUint(fieldElement, 0, 32);
+        uint a = sliceToUint(fieldElement, 0, 16);
+        uint b = sliceToUint(fieldElement, 16, 48);
         return Fp(a, b);
     }
 
-    function hashToField(bytes32 message) private view returns (Fp2[2] memory result) {
+    // NOTE: function is exposed for testing...
+    function hashToField(bytes32 message) public view returns (Fp2[2] memory result) {
         bytes memory some_bytes = expandMessage(message);
         result[0] = Fp2(
             convertSliceToFp(some_bytes, 0, 64),
@@ -215,7 +222,15 @@ library BLSSignature {
         );
     }
 
-    function mapToCurve(Fp2 memory input) private view returns (G2Point memory result) {
+    function mapToCurve(Fp2 memory fieldElement) public view returns (G2Point memory result) {
+        uint[4] memory input;
+        input[0] = fieldElement.a.a;
+        input[1] = fieldElement.a.b;
+        input[2] = fieldElement.b.a;
+        input[3] = fieldElement.b.b;
+
+        uint[8] memory output;
+
         bool success;
         assembly {
             success := staticcall(
@@ -223,16 +238,27 @@ library BLSSignature {
                 BLS12_381_MAP_FIELD_TO_CURVE_PRECOMPILE_ADDRESS,
                 input,
                 128,
-                result,
+                output,
                 256
             )
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
         require(success, "call to map to curve precompile failed");
+
+        return G2Point(
+            Fp2(
+                Fp(output[0], output[1]),
+                Fp(output[2], output[3])
+            ),
+            Fp2(
+                Fp(output[4], output[5]),
+                Fp(output[6], output[7])
+            )
+        );
     }
 
-    function addG2(G2Point memory a, G2Point memory b) private view returns (G2Point memory result) {
+    function addG2(G2Point memory a, G2Point memory b) private view returns (G2Point memory) {
         uint[16] memory input;
         input[0]  = a.X.a.a;
         input[1]  = a.X.a.b;
@@ -252,6 +278,8 @@ library BLSSignature {
         input[14] = b.Y.b.a;
         input[15] = b.Y.b.b;
 
+        uint[8] memory output;
+
         bool success;
         assembly {
             success := staticcall(
@@ -259,39 +287,69 @@ library BLSSignature {
                 BLS12_381_G2_ADD_ADDRESS,
                 input,
                 512,
-                result,
+                output,
                 256
             )
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
         require(success, "call to addition in G2 precompile failed");
+
+        return G2Point(
+            Fp2(
+                Fp(output[0], output[1]),
+                Fp(output[2], output[3])
+            ),
+            Fp2(
+                Fp(output[4], output[5]),
+                Fp(output[6], output[7])
+            )
+        );
     }
 
     // Implements "hash to the curve" from the IETF BLS draft.
-    function hashToCurve(bytes32 message) private view returns (G2Point memory) {
+    // NOTE: function is exposed for testing...
+    function hashToCurve(bytes32 message) public view returns (G2Point memory) {
         Fp2[2] memory messageElementsInField = hashToField(message);
         G2Point memory firstPoint = mapToCurve(messageElementsInField[0]);
         G2Point memory secondPoint = mapToCurve(messageElementsInField[1]);
         return addG2(firstPoint, secondPoint);
     }
 
-    function pairing(G1Point memory u, G2Point memory v) private view returns (bytes32 result) {
-        uint[12] memory input;
+    // NOTE: function is exposed for testing...
+    function blsPairingCheck(G1Point memory publicKey, G2Point memory messageOnCurve, G2Point memory signature) public view returns (bool) {
+        uint[24] memory input;
 
-        input[0] =  u.X.a;
-        input[1] =  u.X.b;
-        input[2] =  u.Y.a;
-        input[3] =  u.Y.b;
+        input[0] =  publicKey.X.a;
+        input[1] =  publicKey.X.b;
+        input[2] =  publicKey.Y.a;
+        input[3] =  publicKey.Y.b;
 
-        input[4] =  v.X.a.a;
-        input[5] =  v.X.a.b;
-        input[6] =  v.X.b.a;
-        input[7] =  v.X.b.b;
-        input[8] =  v.Y.a.a;
-        input[9] =  v.Y.a.b;
-        input[10] = v.Y.b.a;
-        input[11] = v.Y.b.b;
+        input[4] =  messageOnCurve.X.a.a;
+        input[5] =  messageOnCurve.X.a.b;
+        input[6] =  messageOnCurve.X.b.a;
+        input[7] =  messageOnCurve.X.b.b;
+        input[8] =  messageOnCurve.Y.a.a;
+        input[9] =  messageOnCurve.Y.a.b;
+        input[10] = messageOnCurve.Y.b.a;
+        input[11] = messageOnCurve.Y.b.b;
+
+        // NOTE: this constant is -P1, where P1 is the generator of the group G1.
+        input[12] = 31827880280837800241567138048534752271;
+        input[13] = 88385725958748408079899006800036250932223001591707578097800747617502997169851;
+        input[14] = 22997279242622214937712647648895181298;
+        input[15] = 46816884707101390882112958134453447585552332943769894357249934112654335001290;
+
+        input[16] =  signature.X.a.a;
+        input[17] =  signature.X.a.b;
+        input[18] =  signature.X.b.a;
+        input[19] =  signature.X.b.b;
+        input[20] =  signature.Y.a.a;
+        input[21] =  signature.Y.a.b;
+        input[22] =  signature.Y.b.a;
+        input[23] =  signature.Y.b.b;
+
+        uint[1] memory output;
 
         bool success;
         assembly {
@@ -299,96 +357,75 @@ library BLSSignature {
                 sub(gas(), 2000),
                 BLS12_381_PAIRING_PRECOMPILE_ADDRESS,
                 input,
-                384,
-                result,
+                768,
+                output,
                 32
             )
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
         require(success, "call to pairing precompile failed");
-    }
 
-    // Return -P1, negative of the generator of the group G1.
-    // NOTE: computed via: for G1 = (x,y), -G1 = (x, -y)
-    function negativeP1() private pure returns (G1Point memory) {
-        return G1Point(
-            Fp(
-                31827880280837800241567138048534752271,
-                88385725958748408079899006800036250932223001591707578097800747617502997169851
-            ),
-            Fp(
-                22997279242622214937712647648895181298,
-                46816884707101390882112958134453447585552332943769894357249934112654335001290
-            )
-        );
+        return output[0] == 1;
     }
 
     function decodeG1Point(bytes memory encodedX, Fp memory Y) private pure returns (G1Point memory) {
-        uint a = sliceToUint(encodedX, 32, 48);
-        uint b = sliceToUint(encodedX, 0, 32);
+        encodedX[0] = encodedX[0] & BLS_BYTE_WITHOUT_FLAGS_MASK;
+        uint a = sliceToUint(encodedX, 0, 16);
+        uint b = sliceToUint(encodedX, 16, 48);
         Fp memory X = Fp(a, b);
         return G1Point(X,Y);
     }
 
     function decodeG2Point(bytes memory encodedX, Fp2 memory Y) private pure returns (G2Point memory) {
-        uint aa = sliceToUint(encodedX, 32, 48);
-        uint ab = sliceToUint(encodedX, 0, 32);
-        uint ba = sliceToUint(encodedX, 80, 96);
-        uint bb = sliceToUint(encodedX, 48, 80);
+        encodedX[0] = encodedX[0] & BLS_BYTE_WITHOUT_FLAGS_MASK;
+        // NOTE: the "flag bits" of the second half of `encodedX` are always == 0x0
+        uint aa = sliceToUint(encodedX, 0, 16);
+        uint ab = sliceToUint(encodedX, 16, 48);
+        uint ba = sliceToUint(encodedX, 48, 64);
+        uint bb = sliceToUint(encodedX, 64, 96);
+        // NOTE: order is important here for decoding point...
         Fp2 memory X = Fp2(
-            Fp(aa, ab),
-            Fp(ba, bb)
+            Fp(ba, bb),
+            Fp(aa, ab)
         );
         return G2Point(X, Y);
     }
 
-    function isValid(
+    // NOTE: function is exposed for testing...
+    function blsSignatureIsValid(
         bytes32 message,
         bytes memory encodedPublicKey,
         bytes memory encodedSignature,
         Fp memory publicKeyYCoordinate,
         Fp2 memory signatureYCoordinate
-    ) internal view returns (bool) {
+    ) public view returns (bool) {
         G1Point memory publicKey = decodeG1Point(encodedPublicKey, publicKeyYCoordinate);
         G2Point memory signature = decodeG2Point(encodedSignature, signatureYCoordinate);
         G2Point memory messageOnCurve = hashToCurve(message);
 
-        bytes32 firstPairing = pairing(publicKey, messageOnCurve);
-        bytes32 secondPairing = pairing(negativeP1(), signature);
-        return (uint(firstPairing) * uint(secondPairing)) == 1;
-    }
-}
-
-contract DepositContractProxy  {
-    uint constant PUBLIC_KEY_LENGTH = 48;
-    uint constant SIGNATURE_LENGTH = 96;
-
-    DepositContract depositContract;
-
-    constructor(address depositContractAddress) public {
-        depositContract = DepositContract(depositContractAddress);
+        return blsPairingCheck(publicKey, messageOnCurve, signature);
     }
 
     function verifyAndDeposit(
-        bytes memory publicKey,
+        bytes calldata publicKey,
         bytes32 withdrawalCredentials,
-        bytes memory signature,
+        bytes calldata signature,
         bytes32 depositDataRoot,
-        BLSSignature.Fp memory publicKeyYCoordinate,
-        BLSSignature.Fp2 memory signatureYCoordinate
+        Fp calldata publicKeyYCoordinate,
+        Fp2 calldata signatureYCoordinate
     ) public payable {
         require(publicKey.length == PUBLIC_KEY_LENGTH, "incorrectly sized public key");
         require(signature.length == SIGNATURE_LENGTH, "incorrectly sized signature");
 
-        bytes32 signingRoot = DepositSSZ.computeSigningRoot(
+        bytes32 signingRoot = computeSigningRoot(
             publicKey,
             withdrawalCredentials,
             msg.value
         );
 
         require(
-            BLSSignature.isValid(
+            blsSignatureIsValid(
                 signingRoot,
                 publicKey,
                 signature,
@@ -398,11 +435,30 @@ contract DepositContractProxy  {
             "BLS signature verification failed"
         );
 
-        depositContract.deposit{value: msg.value}(
+
+        // NOTE: old vyper contract expects a previous (?) version of ABI encoding
+        // which we manually encode here. it also seems that we can only put a limited number
+        // of arguments into `abi.encodePacked` so we have to break into pieces.
+        bytes memory prefix = abi.encodePacked(
+            depositContract.deposit.selector,
+            uint(0x80),
+            uint(0xe0),
+            uint(0x120),
+            depositDataRoot,
+            uint(0x30),
             publicKey,
-            withdrawalCredentials,
-            signature,
-            depositDataRoot
+            uint128(0x0),
+            uint(0x20)
         );
+
+        (bool success,) = address(depositContract).call{value: msg.value}(
+            abi.encodePacked(
+                prefix,
+                withdrawalCredentials,
+                uint(0x60),
+                signature
+            )
+        );
+        require(success, "call to deposit contract failed");
     }
 }
---
+++ tests/conftest.py
@@ -0,0 +1,173 @@
+import hashlib
+import json
+import os
+
+import eth_tester
+import pytest
+from eth2spec.phase0.spec import (
+    DOMAIN_DEPOSIT,
+    DepositMessage,
+    DepositData,
+    compute_domain,
+    compute_signing_root,
+)
+from eth2spec.utils.ssz.ssz_impl import hash_tree_root
+from eth.vm.forks.berlin import BerlinVM
+from eth_tester import EthereumTester, PyEVMBackend
+from py_ecc.bls.ciphersuites import G2ProofOfPossession
+from py_ecc.bls.g2_primatives import pubkey_to_G1, signature_to_G2
+from py_ecc.optimized_bls12_381.optimized_curve import normalize
+from web3 import Web3
+from web3.providers.eth_tester import EthereumTesterProvider
+
+DIR = os.path.dirname(__file__)
+
+
+def get_deposit_contract_json():
+    file_path = os.path.join(DIR, "../contracts/validator_registration.json")
+    deposit_contract_json = open(file_path).read()
+    return json.loads(deposit_contract_json)
+
+
+def get_proxy_contract_abi():
+    file_path = os.path.join(DIR, "../contracts/DepositContractProxy.abi")
+    with open(file_path) as f:
+        return json.load(f)
+
+
+def get_proxy_contract_bytecode():
+    file_path = os.path.join(DIR, "../contracts/DepositContractProxy.bin")
+    with open(file_path) as f:
+        return f.read()
+
+
+@pytest.fixture
+def berlin_vm_configuration():
+    return ((0, BerlinVM),)
+
+
+@pytest.fixture
+def tester(berlin_vm_configuration):
+    return EthereumTester(PyEVMBackend(vm_configuration=berlin_vm_configuration))
+
+
+@pytest.fixture
+def w3(tester):
+    web3 = Web3(EthereumTesterProvider(tester))
+    return web3
+
+
+@pytest.fixture
+def deposit_contract(w3):
+    contract_bytecode = get_deposit_contract_json()["bytecode"]
+    contract_abi = get_deposit_contract_json()["abi"]
+    registration = w3.eth.contract(abi=contract_abi, bytecode=contract_bytecode)
+    tx_hash = registration.constructor().transact()
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    registration_deployed = w3.eth.contract(
+        address=tx_receipt.contractAddress, abi=contract_abi
+    )
+    return registration_deployed
+
+
+@pytest.fixture
+def proxy_contract(w3, deposit_contract):
+    contract_bytecode = get_proxy_contract_bytecode()
+    contract_abi = get_proxy_contract_abi()
+    proxy = w3.eth.contract(abi=contract_abi, bytecode=contract_bytecode)
+    tx_hash = proxy.constructor(deposit_contract.address).transact()
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    proxy_deployed = w3.eth.contract(
+        address=tx_receipt.contractAddress, abi=contract_abi
+    )
+    return proxy_deployed
+
+
+@pytest.fixture
+def assert_tx_failed(tester):
+    def assert_tx_failed(
+        function_to_test, exception=eth_tester.exceptions.TransactionFailed
+    ):
+        snapshot_id = tester.take_snapshot()
+        with pytest.raises(exception):
+            function_to_test()
+        tester.revert_to_snapshot(snapshot_id)
+
+    return assert_tx_failed
+
+
+@pytest.fixture
+def seed():
+    return "some-secret".encode()
+
+
+@pytest.fixture
+def bls_private_key(seed):
+    return G2ProofOfPossession.KeyGen(seed)
+
+
+@pytest.fixture
+def bls_public_key(bls_private_key):
+    return G2ProofOfPossession.SkToPk(bls_private_key)
+
+
+@pytest.fixture
+def withdrawal_credentials():
+    return b"\x00" + hashlib.sha256("some-secret".encode()).digest()[1:]
+
+
+@pytest.fixture
+def deposit_amount():
+    # NOTE: 32 ETH in Gwei
+    return 32 * 10 ** 9
+
+
+@pytest.fixture
+def deposit_message(bls_public_key, withdrawal_credentials, deposit_amount):
+    return DepositMessage(
+        pubkey=bls_public_key,
+        withdrawal_credentials=withdrawal_credentials,
+        amount=deposit_amount,
+    )
+
+
+@pytest.fixture
+def signing_root(deposit_message):
+    domain = compute_domain(DOMAIN_DEPOSIT)
+    return compute_signing_root(deposit_message, domain)
+
+
+@pytest.fixture
+def deposit_data_root(deposit_message, signature):
+    deposit_data = DepositData(
+        pubkey=deposit_message.pubkey,
+        withdrawal_credentials=deposit_message.withdrawal_credentials,
+        amount=deposit_message.amount,
+        signature=signature,
+    )
+    root = hash_tree_root(deposit_data)
+    return root
+
+
+@pytest.fixture
+def signature(bls_private_key, signing_root):
+    return G2ProofOfPossession.Sign(bls_private_key, signing_root)
+
+
+@pytest.fixture
+def public_key_witness(bls_public_key):
+    group_element = pubkey_to_G1(bls_public_key)
+    normalized_group_element = normalize(group_element)
+    return normalized_group_element[1]
+
+
+@pytest.fixture
+def signature_witness(signature):
+    group_element = signature_to_G2(signature)
+    normalized_group_element = normalize(group_element)
+    return normalized_group_element[1]
+
+
+@pytest.fixture
+def dst():
+    return G2ProofOfPossession.DST
